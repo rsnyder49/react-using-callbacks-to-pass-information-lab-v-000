306be157b9c69eea39fee3dedaaae8ff1a184bac
Robert Snyder <r.snyder49@gmail.com>
2019-01-10 01:11:38 +0000

    Automatically backed up by Learn

--- src/Cell.js
+++ src/Cell.js
@@ -19,7 +19,7 @@ export default class Cell extends Component {
   render() {
     return (
       <div className="cell" 
-      style={{backgroundColor: this.state.color}}>
+      style={{backgroundColor: this.state.color}}
       onClick={this.handleClick}>
       </div>
     )
